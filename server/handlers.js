a7d75d9dfd6cf0cf562535259a52565f34271074
oliverjam <oliverjamesphillips@gmail.com>
2020-02-16 22:31:39 +0000

    Add post upload form

--- server/handlers.js
+++ server/handlers.js
@@ -1,7 +1,16 @@
 const layout = require("./templates/layout");
 
 function home(request, response) {
-  const html = layout(`<h1>Hello world</h1>`);
+  const html = layout(`
+    <h1>Write post</h1>
+    <form action="/create-post" method="POST">
+      <label for="title">Title</label>
+      <input id="title" name="title">
+      <label for="body">Body</label>
+      <textarea id="body" name="body"></textarea>
+      <button type="submit">Post</button>
+    </form>
+  `);
   response.end(html);
 }
 
