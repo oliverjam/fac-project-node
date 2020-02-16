68586084e511e483bae5be22c485fd847340f001
oliverjam <oliverjamesphillips@gmail.com>
2020-02-16 22:46:30 +0000

    Show links to all posts on homepage

--- server/handlers.js
+++ server/handlers.js
@@ -2,6 +2,7 @@ const layout = require("./templates/layout");
 const db = require("./db");
 
 function home(request, response) {
+  const posts = db.get();
   const html = layout(`
     <h1>Write post</h1>
     <form action="/create-post" method="POST">
@@ -11,6 +12,12 @@ function home(request, response) {
       <textarea id="body" name="body"></textarea>
       <button type="submit">Post</button>
     </form>
+    <h2>Posts</h2>
+    <ul>
+      ${posts.map(
+        post => `<li><a href="/post/${post.id}">${post.title}</a></li>`
+      )}
+    </h2>
   `);
   response.end(html);
 }
