929dda9e434d3fbc7ad077ffadb9bc46ef958971
oliverjam <oliverjamesphillips@gmail.com>
2020-02-17 19:47:31 +0000

    Split routing out into separate file
    To make it easier to test the server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,6 @@
 const { createServer } = require("http");
-const handlers = require("./handlers");
+const router = require("./router");
 
-const server = createServer((request, response) => {
-  const url = request.url;
-  if (url === "/") {
-    handlers.home(request, response);
-  } else if (url === "/create-post") {
-    handlers.createPost(request, response);
-  } else if (url.startsWith("/post")) {
-    handlers.post(request, response);
-  } else if (url.startsWith("/remove")) {
-    handlers.removePost(request, response);
-  } else if (url.startsWith("/assets")) {
-    handlers.assets(request, response);
-  } else {
-    handlers.notFound(request, response);
-  }
-});
+const server = createServer(router);
 
 server.listen(3000, () => console.log("Listening on http://localhost:3000"));
---
+++ server/router.js
@@ -0,0 +1,20 @@
+const handlers = require("./handlers");
+
+function router(request, response) {
+  const url = request.url;
+  if (url === "/") {
+    handlers.home(request, response);
+  } else if (url === "/create-post") {
+    handlers.createPost(request, response);
+  } else if (url.startsWith("/post")) {
+    handlers.post(request, response);
+  } else if (url.startsWith("/remove")) {
+    handlers.removePost(request, response);
+  } else if (url.startsWith("/assets")) {
+    handlers.assets(request, response);
+  } else {
+    handlers.notFound(request, response);
+  }
+}
+
+module.exports = router;
