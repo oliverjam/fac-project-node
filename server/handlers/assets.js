bcd3ca5befad900d8b0d0e5e1f43287efc43df12
oliverjam <oliverjamesphillips@gmail.com>
2020-02-17 10:53:39 +0000

    Add favicon

--- server/handlers/assets.js
+++ server/handlers/assets.js
@@ -3,13 +3,13 @@ const fs = require("fs").promises;
 
 const types = {
   css: "text/css",
+  ico: "image/x-icon",
 };
 
 function assets(request, response) {
   const [, , fileName] = request.url.split("/");
   const [, ext] = fileName.split(".");
   const type = types[ext];
-
   fs.readFile(path.join(__dirname, "..", "assets", fileName))
     .then(content => {
       response.writeHead(200, { "content-type": type });
--- server/templates/layout.js
+++ server/templates/layout.js
@@ -6,6 +6,7 @@ function layout(content) {
       <meta charset="utf-8">
       <title>Test</title>
       <meta name="viewport" content="width=device-width">
+      <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
       <link rel="stylesheet" href="/assets/styles.css">
     </head>
     <body>
